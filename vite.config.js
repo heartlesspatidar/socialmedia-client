ec664c367496690f777389e380570b2071f394f2
heartlesspatidar <npdnitin9@gmail.com>
2025-01-20 20:06:24 +0530

    uppp

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // Dynamic base path configuration
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/socialmedia/' : '/', 
+  base: process.env.NODE_ENV === 'production' ? '/socialmedia-client/' : '/', 
   server: {
     port: process.env.VITE_PORT || 5173,
   },
